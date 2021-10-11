224ef22c44809320985d9b0e41173563e2cf076f
Maxime Paletou <maxime.paletou@icloud.com>
2021-10-11 09:33:52 +0200

    Corrections orthographiques

--- src/app/services/portfolio.service.ts
+++ src/app/services/portfolio.service.ts
@@ -12,7 +12,7 @@ export class PortfolioService{
       name: "PaletouMaxi.me",
       logo: "../../../../assets/images/LogoSiteMax.png",
       technos: "Angular, NodeJS",
-      description: "PaletouMaxi.me est mon site personnel réalisé afin de présenter mes compétences et les différents projets auxquels j'ai participé. Le site a été réalisé avec Angular connecté à une API NodeJS et une base de données NoSQL MangoDB",
+      description: "PaletouMaxi.me est mon site personnel réalisé afin de présenter mes compétences et les différents projets auxquels j'ai participé. Le site a été réalisé avec Angular connecté à une API NodeJS et une base de données MySQL",
       libraryImages: [
         {
           image: "../../../../assets/images/imageSites/paletoumaxdotme/pmmeimg1.png"
@@ -27,7 +27,7 @@ export class PortfolioService{
       name: "GASPRA V2",
       logo: "../../../../assets/images/GaspraV2.png",
       technos: "HTML, CSS, JS, LabView",
-      description: "<p> <b>GASPRA V2</b> est une interface de pilotage de sonde utilisé dans les moteurs en développement chez <b>Safran</b>. Cette interface est intégrée au serveur web de l'automate pilotant la sonde. <br> <br> Le but de cette interface est de récupérer les données renvoyées par la sonde, les traduires sous forme de graphique représentant le balayage de la sonde dans son environnement ainsi que les valeur associées. <br> <br> La sonde est pilotable depuis l'interface de manière automatique ou manuelle. L'interface permet également de régler différents paramètres de la sonde.</p>",
+      description: "<p> <b>GASPRA V2</b> est une interface de pilotage de sonde utilisée dans les moteurs en développement chez <b>Safran</b>. Cette interface est intégrée au serveur web de l'automate pilotant la sonde. <br> <br> Le but de cette interface est de récupérer les données renvoyées par la sonde, les traduires sous forme de graphique représentant le balayage de la sonde dans son environnement ainsi que les valeurs associées. <br> <br> La sonde est pilotable depuis l'interface de manière automatique ou manuelle. L'interface permet également de régler différents paramètres de la sonde.</p>",
       libraryImages: [
         {
           image: "../../../../assets/images/imageSites/gasprav2/1.png"
@@ -60,7 +60,7 @@ export class PortfolioService{
       name: "Football Manager",
       logo: "../../../../assets/images/portfolio_logo/footLogo.png",
       technos: "Symfony, MySQL",
-      description: "<p><b>FootBall Manager</b> est un projet réalisé lors de mes études qui avait pour but de gérer l'ensemble des matchs de ligue <br/><br/> Les entraineurs de chacun des équipe dispose d'un accès au site leur permettant de gérer leur équipe et définir les joueurs pour chacun des prochains matchs ainsi que leur positions et postes sur le terrain. <br/> Les commentateurs ont la possibilité de créer un match en définissant le lieu,la date et les arbitres. Le match est lancé à la date créée et les commentateur renseignent en direct les différents moments forts du match (buts, cartons). <br/> À la fin du match, un résumé est disponible au format PDF comportant l'ensemble des informations du match (joueurs, scores, cartons,...)</p>",
+      description: "<p><b>FootBall Manager</b> est un projet réalisé lors de mes études qui avait pour but de gérer l'ensemble des matchs de ligue 1 <br/><br/> Les entraineurs de chacune des équipes disposent d'un accès au site leur permettant de gérer leur équipe et définir les joueurs pour chacun des prochains matchs ainsi que leurs positions et postes sur le terrain. <br/> Les commentateurs ont la possibilité de créer un match en définissant le lieu,la date et les arbitres. Le match est lancé à la date créée et les commentateurs renseignent en direct les différents moments forts du match (buts, cartons). <br/> À la fin du match, un résumé est disponible au format PDF comportant l'ensemble des informations du match (joueurs, scores, cartons,...)</p>",
       libraryImages: [
         {
           image: "../../../../assets/images/imageSites/projetFoot/1.png"
@@ -75,7 +75,7 @@ export class PortfolioService{
       name: "Madera",
       logo: "../../../../assets/images/portfolio_logo/logo2.2.2.png",
       technos: "Symfony, Kotlin",
-      description: "<p><b>MADERA</b> est un projet scolaire réalisé lors de mon alternance au CESI. L'interface permet la création de plans de maisons modulaires pour les particuliers et les professsionnels. <br> <br> <u>Les fonctionnalités :</u> <ul><li>Gestion des clients</li><li>Gestion des projets/chantier</li> <li>Création des plans modulaires</li></ul></p> <h2> L'application mobile </h2> <p>Le projet est également composée d'une application mobile utilisée sur tablette par les commerciaux afin de pouvoir gérer les projets et créer des plans. L'application dispose d'un mode <b>hors ligne</b> permettant aux commerciaux de travailler même sans accès réseau.</p>",
+      description: "<p><b>MADERA</b> est un projet scolaire réalisé lors de mon alternance au CESI. L'interface permet la création de plans de maisons modulaires pour les particuliers et les professsionnels. <br> <br> <u>Les fonctionnalités :</u> <ul><li>Gestion des clients</li><li>Gestion des projets/chantiers</li> <li>Création des plans modulaires</li></ul></p> <h2> L'application mobile </h2> <p>Le projet est également composé d'une application mobile utilisée sur tablette par les commerciaux afin de pouvoir gérer les projets et créer des plans. L'application dispose d'un mode <b>hors ligne</b> permettant aux commerciaux de travailler même sans accès réseau.</p>",
       libraryImages: [
         {
           image: "../../../../assets/images/imageSites/madera/1.png"
